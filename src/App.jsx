7423143432f3409a1c90ba6612a052a67870e861
Zafar Alam <sheikhzafar2664@gmail.com>
2024-05-29 21:35:21 +0530

    completed first project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css"
+
+function App() {
+  
+
+  return (
+   <div className="main">
+    <h1 className="center">Learn Web Development</h1>
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Commodi facere autem praesentium, aliquid provident tempore placeat non voluptatibus eius, 
+        molestias rem quis id quasi ducimus aliquam cupiditate deleniti sit officia.</p>
+    </div>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Iste hic quas vitae reprehenderit est itaque! Nulla quis maxime excepturi quos culpa
+         repudiandae distinctio iusto! Illum obcaecati odit voluptatem dolorum reprehenderit.
+      </p>
+    </div>
+    <div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+         Nobis alias esse perspiciatis ut id vero reiciendis assumenda dolore repudiandae sint ex, 
+         vel voluptas obcaecati ipsam atque similique, possimus, quo illo.</p>
+    </div>
+   </div>
+      
+  )
+}
+
+export default App
